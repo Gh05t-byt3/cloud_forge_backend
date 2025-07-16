5d123db2ce89f3fcf4db49dd05b257daa78dcf58
mojo_jojo <mojo@jojo.com>
2025-07-16 20:40:17 +0000

    to be overwritten

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,20 @@ import { swagger } from "@elysiajs/swagger";
 import { vmRouter } from "./vms";
 
 const app = new Elysia()
-    app.use(swagger())
+    app.use(swagger({
+      documentation: {
+        info: {
+          title: 'CloudForge API',
+          version: '1.0.0'
+        },
+        servers: [{
+          url: 'http://localhost:3000',
+          description: 'Local development Server'
+        }]
+      }
+    }))
     app.use(vmRouter)
+    
     app.get("/", () => "Hello Elysia")
     app.get("/health", () => {
       return {
