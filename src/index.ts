ed81380232ea4d7321b46dc3b1453ef29d34f826
mojo_jojo <mojo@jojo.com>
2025-07-16 20:52:42 +0000

    meh

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,7 @@ import { swagger } from "@elysiajs/swagger";
 import { vmRouter } from "./vms";
 
 const app = new Elysia()
-    app.use(swagger({
-      documentation: {
-        info: {
-          title: 'CloudForge API',
-          version: '1.0.0'
-        },
-        servers: [{
-          url: 'http://localhost:3000',
-          description: 'Local development Server'
-        }]
-      }
-    }))
+    app.use(swagger())
     app.use(vmRouter)
     
     app.get("/", () => "Hello Elysia")
