cf99d603bc1f829dd6fbdc1c9912b788ea220470
Etornam <navidben45@gmail.com>
2025-07-16 20:33:13 +0000

    Change swagger UI configurations

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import { swagger } from "@elysiajs/swagger";
 import { vmRouter } from "./vms";
 
 const app = new Elysia()
-    app.use(swagger())
+    app.use(swagger({
+      provider: "swagger-ui"
+    }))
     app.use(vmRouter)
     app.get("/", () => "Hello Elysia").listen(3000);
     app.get("/health", () => {
